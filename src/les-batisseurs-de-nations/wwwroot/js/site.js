2ddf93c52e2d55b3c0c741c9aed4c47313e6d78c
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-08-07 11:14:12 -0400

    Add channels

---
+++ src/les-batisseurs-de-nations/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿function twitchLive(elementId, channelName) {
+    new Twitch.Embed(elementId, {
+        //width: 854,
+        //height: 480,
+        channel: channelName,
+        // Only needed if this page is going to be embedded on other websites
+        //parent: ["embed.example.com", "othersite.example.com"]
+    });
+}
