7d99d713e5d05def2825d2824dbcc9008fac9310
Carl-Hugo <Carl-Hugo@users.noreply.github.com>
2021-08-08 05:03:30 +0000

    Deploying to gh-pages from  @ f54c87183c623d8c52cf6901f5a41846f75508b7 🚀

---
+++ js/site.js
@@ -0,0 +1,15 @@
+﻿var lbdnTwitchEmbed = {};
+
+function twitchLive(elementId, channelName) {
+    lbdnTwitchEmbed = new Twitch.Embed(elementId, {
+        //width: 854,
+        //height: 480,
+        channel: channelName,
+        // Only needed if this page is going to be embedded on other websites
+        //parent: ["embed.example.com", "othersite.example.com"]
+    });
+}
+
+function setChannel(channelName) {
+    lbdnTwitchEmbed.setChannel(channelName);
+}
